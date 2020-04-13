401008b243bdf84a35a393212c4ec6468262f4a2
hamidhadi <hamidhadi@live.com>
2020-04-13 18:33:17 +0100

    Add screen types for example project

---
+++ example/src/screens/types.ts
@@ -0,0 +1,10 @@
+export enum ScreenNames {
+  HomeScreen = 'example.homeScreen',
+  StretchyHeaderScreen = 'example.stretchyHeaderScreen',
+  StretchyFlatListScreen = 'example.stretchyFlatListScreen',
+  StretchySectionListScreen = 'example.stretchySectionListScreen',
+}
+
+export interface ScreenProps {
+  componentId: string;
+}
